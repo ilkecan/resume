9feb51163f7f9cfad2065a790c220689db8aae56
İlkecan Bozdoğan <ilkecan@protonmail.com>
2026-07-31 21:12:17 +0300

    flake: update inputs

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "A Typst project that uses Typst packages";
 
   inputs = {
-    nixpkgs.url = "https://channels.nixos.org/nixos-unstable/nixexprs.tar.xz";
+    nixpkgs.url = "https://channels.nixos.org/nixpkgs-unstable/nixexprs.tar.xz";
 
     typix = {
       url = "github:loqusion/typix";
